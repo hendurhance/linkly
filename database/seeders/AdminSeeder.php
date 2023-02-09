efe2617346416233ccb0eae703227c100b196044
Endurance, the Martian <afriviral@gmail.com>
2023-02-09 11:46:34 +0100

    Getting Admin Account Started

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::factory(1)->create([
+            'name' => 'System Admin',
+            'email' => 'admin@linkly.io'
+        ]);
+    }
+}
